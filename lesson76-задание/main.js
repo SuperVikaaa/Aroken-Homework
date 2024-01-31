532afc72e106337f3be5738688a3c92145f32d86
SuperVikaaa <153988571+SuperVikaaa@users.noreply.github.com>
2024-01-31 16:51:25 +0300

    add lesson76

---
+++ lesson76-задание/main.js
@@ -0,0 +1,17 @@
+const firstName = 'Viktoria'
+const lastName = 'Tsepennikova'
+
+console.log(firstName, lastName)
+
+let age = '32'
+console.log(age)
+
+const gender = 'Woman'
+console.log(gender)
+
+const nationality = 'Russian'
+
+console.log(nationality)
+
+let occupation = 'Study'
+console.log(occupation)
