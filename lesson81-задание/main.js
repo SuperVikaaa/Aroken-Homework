b6270c3c86edb0c5e0cc1240cdfd9f3a784c9f9b
SuperVikaaa <153988571+SuperVikaaa@users.noreply.github.com>
2024-02-06 12:46:22 +0300

    add lesson81

---
+++ lesson81-задание/main.js
@@ -0,0 +1,15 @@
+// const numbers = [1, 2, 3, 4, 5, 6]
+
+// for (let i=0; i < numbers.length; i++) {
+//   if (i % 2 == 0) {
+//     console.log(numbers[i] + 1)
+//   }
+// }  
+
+// ======================================================//
+
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'darkBlue', 'purple']
+
+for (i = colors.length-1; i >=0; i--) {
+  console.log(colors[i])
+}
